297ea4d832d1b7fcc4186bf3dfa054840965831f
Bec0l <rebeccacaoliveira@gmail.com>
2023-04-05 07:47:23 -0300

    Navbar update

--- Consultant Page/sueli-campos/src/App.js
+++ Consultant Page/sueli-campos/src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Footer from './Components/Footer';
-import Header from './Components/Header';
+import ResponsiveAppBar from './Components/Header';
 import Biography from './Components/Biography';
-import { Container } from '@mui/material';
+import { Box, Container } from '@mui/material';
 
 
 function App() {
   return (
     <div className="App">
       <Container>
-      <Header />
+      <Box>
+      <ResponsiveAppBar />
+      </Box>
       <Biography />
       <Footer />
       </Container>
